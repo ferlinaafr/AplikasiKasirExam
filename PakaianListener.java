1948a6f1071840b1ff037974e70f17d599e1e225
ferlinaafr <144145190+ferlinaafr@users.noreply.github.com>
2024-02-04 11:18:25 +0700

    Add files via upload

---
+++ PakaianListener.java
@@ -0,0 +1,16 @@
+package finalprojectBAD;
+
+/**
+ *
+ * @author faisalamir
+ */
+public interface PakaianListener {
+    
+    interface TambahPakaian {
+        void merkSudahAda(String message);
+        
+        void kapasitasPenuh(String message);
+        
+    }
+    
+}
